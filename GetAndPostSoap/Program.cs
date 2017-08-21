c30c0d96410521516a20c82cdde207bf9fb4860b
Chung Nguyen <zrmedia5@gmail.com>
2017-08-21 10:09:49 +0700

    Add project files.

---
+++ GetAndPostSoap/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GetAndPostSoap
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // GetRequest("https://www.facebook.com");
+            PostRequest("https://posttestserver.com/post.php");
+            Console.ReadKey();
+        }
+
+        async static void GetRequest(string url)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                using(HttpResponseMessage response = await client.GetAsync(url))
+                {
+                    using(HttpContent content = response.Content)
+                    {
+                        // string mycontent = await content.ReadAsStringAsync();
+                        HttpContentHeaders headers = content.Headers;
+                        Console.WriteLine(headers);
+                    }                    
+                    
+                }                
+                
+            }
+
+        }
+        async static void PostRequest(string url)
+        {
+            IEnumerable<KeyValuePair<string, string>> queries = new List<KeyValuePair<string, string>>()
+            {
+                new KeyValuePair<string, string>("query1", "chung"),
+                new KeyValuePair<string, string>("query2", "nguyen")
+            };
+
+            HttpContent q = new FormUrlEncodedContent(queries);
+            using (HttpClient client = new HttpClient())
+            {
+                using(HttpResponseMessage response = await client.PostAsync(url,q))
+                {
+                    using(HttpContent content = response.Content)
+                    {
+                        string mycontent = await content.ReadAsStringAsync();
+                        HttpContentHeaders headers = content.Headers;
+                        Console.WriteLine(mycontent);
+                    }
+                }
+            }
+        }
+    }
+}
